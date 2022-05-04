c2ea3607f521a5f2a80f7e0a7a84b6555424abd4
laci <thelastpointer@gmail.com>
2022-05-04 15:12:09 +0200

    adds context menu to replace CanvasScaler; better inspector for CanvasScaler2

--- Runtime/Components/CanvasScaler2.cs
+++ Runtime/Components/CanvasScaler2.cs
@@ -25,7 +25,21 @@ namespace TLP.UI
 
         protected override void Handle()
         {
-            float referenceWidth = (Reference == ReferenceResolution.Resolution1080p ? 1920 : 1280);
+            float referenceWidth = 1920;
+
+            switch (Reference)
+            {
+                case ReferenceResolution.Resolution4K:
+                    referenceWidth = 3840;
+                    break;
+                case ReferenceResolution.Resolution1080p:
+                    referenceWidth = 1920;
+                    break;
+                case ReferenceResolution.Resolution720p:
+                    referenceWidth = 1280;
+                    break;
+            }
+
             this.scaleFactor = ((float)Screen.width / referenceWidth) * additionalScale;
 
             base.Handle();
@@ -43,6 +57,35 @@ namespace TLP.UI
             Resolution1080p,    // 1920x1080
             Resolution720p      // 1280x720
         }
+
+#if UNITY_EDITOR
+        [MenuItem("CONTEXT/CanvasScaler/Replace With CanvasScaler2")]
+        public static void ReplaceBuiltin(MenuCommand command)
+        {
+            var scaler = (command.context as CanvasScaler);
+            if (scaler != null)
+            {
+                ReferenceResolution resolution = ReferenceResolution.Resolution1080p;
+
+                if (scaler.uiScaleMode == ScaleMode.ScaleWithScreenSize)
+                {
+                    if (scaler.referenceResolution.x < 1600)
+                        resolution = ReferenceResolution.Resolution720p;
+                    else if (scaler.referenceResolution.x < 2880)
+                        resolution = ReferenceResolution.Resolution1080p;
+                    else
+                        resolution = ReferenceResolution.Resolution4K;
+                }
+
+                var go = scaler.gameObject;
+
+                DestroyImmediate(scaler);
+
+                var scaler2 = go.AddComponent<CanvasScaler2>();
+                scaler2.Reference = resolution;
+            }
+        }
+#endif
     }
 }
 
@@ -55,7 +98,7 @@ namespace TLP.UI.Editors
     {
         public override void OnInspectorGUI()
         {
-            //EditorGUILayout.PropertyField(serializedObject.FindProperty("Reference"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("Reference"));
             //EditorGUILayout.PropertyField(serializedObject.FindProperty("m_ReferencePixelsPerUnit"));
 
             serializedObject.ApplyModifiedProperties();
