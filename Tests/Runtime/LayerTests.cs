efb2a972f9a2a273720aec7f195bfef72b6fea3e
laci <thelastpointer@gmail.com>
2022-05-04 15:14:46 +0200

    add mock tests

---
+++ Tests/Runtime/LayerTests.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TLP.UI.Tests
+{
+    public class LayerTests
+    {
+        [Test]
+        public void DefaultLayerGetsCreated()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [Test]
+        public void LayerChildrenAdded()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [Test]
+        public void PreCreatedLayers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // --->move to window tests?
+        [Test]
+        public void AutoRegisteredWindows()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
